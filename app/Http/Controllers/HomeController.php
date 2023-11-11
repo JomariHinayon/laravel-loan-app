06c344b9e802640e1b6059d5c11d1bd3c033fd00
Jomari Hinayon <hinayonjomari@gmail.com>
2023-11-12 02:01:11 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function index() {
+        return view('home.userpage');
+    }
+
+    public function redirect() {
+        $user_type=Auth::user()->user_type;
+
+        if ($user_type == '1') {
+            return view('admin.home');
+        } else {
+            return view('dashboard');
+        }
+    }
+}
